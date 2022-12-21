5e5491e781200dc256049d20fe0cad9380f00af3
Iga Ryś <imirys96@gmail.com>
2022-12-21 20:09:45 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 <script src="/js/modal.js"></script>;
 <script src="/js/location.js"></script>;
 <script src="/js/frenchise-popup.js"></script>;
-<script src="/js/swiper.js"></script>;
+<script src="/js/swiper.js"></script>
